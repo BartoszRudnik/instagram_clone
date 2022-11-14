9defa954bffb3d7bb807d568ea0f83ebfeba6112
Bartosz Rudnik <rudnik@vialutions.local>
2022-11-14 09:36:34 +0100

    feat: loading screen

--- lib/views/components/loading/loading_screen.dart
+++ lib/views/components/loading/loading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/views/components/constants/strings.dart';
 import 'package:instagram_clone/views/components/loading/loading_screen_controller.dart';
@@ -10,20 +12,114 @@ class LoadingScreen {
 
   LoadingScreenController? _controller;
 
-  void show({
+  LoadingScreenController? showOverlay({
     required BuildContext context,
-    String text = Strings.loading,
-  }) {}
+    required String text,
+  }) {
+    final state = Overlay.of(context);
+
+    if (state == null) {
+      return null;
+    }
+
+    final textController = StreamController<String>();
+
+    textController.add(
+      text,
+    );
+
+    final renderBox = context.findRenderObject() as RenderBox;
+    final size = renderBox.size;
+
+    final overlay = OverlayEntry(
+      builder: (context) => Material(
+        color: Colors.black.withAlpha(
+          150,
+        ),
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints(
+              maxWidth: size.width * 0.8,
+              maxHeight: size.height * 0.8,
+              minWidth: size.width * 0.5,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(
+                10,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const CircularProgressIndicator(),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    StreamBuilder<String>(
+                      stream: textController.stream,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(
+                            snapshot.requireData,
+                            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                  color: Colors.black,
+                                ),
+                          );
+                        } else {
+                          return const SizedBox.shrink();
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    state.insert(overlay);
+
+    return LoadingScreenController(
+      close: () {
+        textController.close();
+        overlay.remove();
+
+        return true;
+      },
+      update: (text) {
+        textController.add(text);
+
+        return true;
+      },
+    );
+  }
 
   void hide() {
     _controller?.close();
     _controller = null;
   }
 
-  LoadingScreenController? showOverlay({
+  void show({
     required BuildContext context,
-    required String text,
+    String text = Strings.loading,
   }) {
-    return null;
+    if (_controller?.update(text) ?? false) {
+      return;
+    } else {
+      _controller = showOverlay(
+        context: context,
+        text: text,
+      );
+    }
   }
 }
